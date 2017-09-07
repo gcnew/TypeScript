520d5dc36eaafa86fd89db123dc95a2dd0d51044
gcnew <gcnew@abv.bg>
2017-09-07 16:31:27 +0300

    Accept baselines

--- tests/baselines/reference/nonNull.js
+++ tests/baselines/reference/nonNull.js
@@ -21,11 +21,17 @@ type KeyOfBang<T> = keyof T!
 type Strip1<T> = { [K in keyof T!]: T![K] }
 type Strip2<T> = { [K in keyof T!]: T![K]! }
 
+type Error1<T, K extends keyof T!> = T[K]
+
+type StupidButOK<T>  = { [K in keyof T]: T![K] }
+type StupidButOK2<T> = { [K in keyof T]: T![K!] }
+
 type Obj = {
     x: number|null,
-    z: { kind: 'Just', value: string } |
-       { kind: 'Nothing' } |
-       undefined
+
+    z:   { kind: 'Just', value: string }
+       | { kind: 'Nothing' }
+       | undefined
 }
 
 type T1 = number!
@@ -47,9 +53,6 @@ type T16 = MapHasKey<Obj, 'kind'>
 type T17 = Strip1<Obj>
 type T18 = Strip1<Obj|undefined>
 type T19 = Strip2<Obj|undefined>
-
-
-type Error1<T, K extends keyof T!> = T[K]
 
 let a: string | undefined | null | never;
 let b: typeof a!;
@@ -89,6 +92,13 @@ infer6({ x: 1, y: 2 });
 infer6({ x: 1, y: 'string' });
 
 function assignability<T>(x: T, y: T!) {
+    const a: T = x;
+    const b: T = y;
+    const c: T! = x;
+    const d: T! = y;
+}
+
+function assignability2<T extends number>(x: T, y: T!) {
     const a: T = x;
     const b: T = y;
     const c: T! = x;
@@ -145,51 +155,196 @@ const wf: typeof w = f;
 const wg: typeof w = g;
 const wh: typeof w = h;
 
-type A    = { a: number }
-type OptA = { a: number | null }
+type A<T>    = { a: T }
+type OptA<T> = { a: T | null }
 
-function knownTypes(a: A, optA: OptA, bangA: A!, bangOptA: OptA!, strictA: StrictMembers<A>, strictOptA: StrictMembers<OptA>) {
-    const a1: A = a;
-    const a2: A = optA;
-    const a3: A = bangA;
-    const a4: A = bangOptA;
-    const a5: A = strictA;
-    const a6: A = strictOptA;
+function knownTypes(
+    a:          A<number>,
+    optA:       OptA<number>,
+    bangA:      A<number>!,
+    bangOptA:   OptA<number>!,
+    strictA:    StrictMembers<A<number>>,
+    strictOptA: StrictMembers<OptA<number>>
+) {
+    const a1: A<number> = a;
+    const a2: A<number> = optA;
+    const a3: A<number> = bangA;
+    const a4: A<number> = bangOptA;
+    const a5: A<number> = strictA;
+    const a6: A<number> = strictOptA;
 
-    const oa1: OptA = a;
-    const oa2: OptA = optA;
-    const oa3: OptA = bangA;
-    const oa4: OptA = bangOptA;
-    const oa5: OptA = strictA;
-    const oa6: OptA = strictOptA;
+    const oa1: OptA<number> = a;
+    const oa2: OptA<number> = optA;
+    const oa3: OptA<number> = bangA;
+    const oa4: OptA<number> = bangOptA;
+    const oa5: OptA<number> = strictA;
+    const oa6: OptA<number> = strictOptA;
 
-    const ba1: A! = a;
-    const ba2: A! = optA;
-    const ba3: A! = bangA;
-    const ba4: A! = bangOptA;
-    const ba5: A! = strictA;
-    const ba6: A! = strictOptA;
+    const ba1: A<number>! = a;
+    const ba2: A<number>! = optA;
+    const ba3: A<number>! = bangA;
+    const ba4: A<number>! = bangOptA;
+    const ba5: A<number>! = strictA;
+    const ba6: A<number>! = strictOptA;
 
-    const boa1: OptA! = a;
-    const boa2: OptA! = optA;
-    const boa3: OptA! = bangA;
-    const boa4: OptA! = bangOptA;
-    const boa5: OptA! = strictA;
-    const boa6: OptA! = strictOptA;
+    const boa1: OptA<number>! = a;
+    const boa2: OptA<number>! = optA;
+    const boa3: OptA<number>! = bangA;
+    const boa4: OptA<number>! = bangOptA;
+    const boa5: OptA<number>! = strictA;
+    const boa6: OptA<number>! = strictOptA;
 
-    const sa1: StrictMembers<A> = a;
-    const sa2: StrictMembers<A> = optA;
-    const sa3: StrictMembers<A> = bangA;
-    const sa4: StrictMembers<A> = bangOptA;
-    const sa5: StrictMembers<A> = strictA;
-    const sa6: StrictMembers<A> = strictOptA;
+    const sa1: StrictMembers<A<number>> = a;
+    const sa2: StrictMembers<A<number>> = optA;
+    const sa3: StrictMembers<A<number>> = bangA;
+    const sa4: StrictMembers<A<number>> = bangOptA;
+    const sa5: StrictMembers<A<number>> = strictA;
+    const sa6: StrictMembers<A<number>> = strictOptA;
 
-    const soa1: StrictMembers<OptA> = a;
-    const soa2: StrictMembers<OptA> = optA;
-    const soa3: StrictMembers<OptA> = bangA;
-    const soa4: StrictMembers<OptA> = bangOptA;
-    const soa5: StrictMembers<OptA> = strictA;
-    const soa6: StrictMembers<OptA> = strictOptA;
+    const soa1: StrictMembers<OptA<number>> = a;
+    const soa2: StrictMembers<OptA<number>> = optA;
+    const soa3: StrictMembers<OptA<number>> = bangA;
+    const soa4: StrictMembers<OptA<number>> = bangOptA;
+    const soa5: StrictMembers<OptA<number>> = strictA;
+    const soa6: StrictMembers<OptA<number>> = strictOptA;
+}
+
+function unknownTypes<T>(
+    a:          A<T>,
+    optA:       OptA<T>,
+    bangA:      A<T>!,
+    bangOptA:   OptA<T>!,
+    strictA:    StrictMembers<A<T>>,
+    strictOptA: StrictMembers<OptA<T>>
+) {
+    const a1: A<T> = a;
+    const a2: A<T> = optA;
+    const a3: A<T> = bangA;
+    const a4: A<T> = bangOptA;
+    const a5: A<T> = strictA;
+    const a6: A<T> = strictOptA;
+
+    const oa1: OptA<T> = a;
+    const oa2: OptA<T> = optA;
+    const oa3: OptA<T> = bangA;
+    const oa4: OptA<T> = bangOptA;
+    const oa5: OptA<T> = strictA;
+    const oa6: OptA<T> = strictOptA;
+
+    const ba1: A<T>! = a;
+    const ba2: A<T>! = optA;
+    const ba3: A<T>! = bangA;
+    const ba4: A<T>! = bangOptA;
+    const ba5: A<T>! = strictA;
+    const ba6: A<T>! = strictOptA;
+
+    const boa1: OptA<T>! = a;
+    const boa2: OptA<T>! = optA;
+    const boa3: OptA<T>! = bangA;
+    const boa4: OptA<T>! = bangOptA;
+    const boa5: OptA<T>! = strictA;
+    const boa6: OptA<T>! = strictOptA;
+
+    const sa1: StrictMembers<A<T>> = a;
+    const sa2: StrictMembers<A<T>> = optA;
+    const sa3: StrictMembers<A<T>> = bangA;
+    const sa4: StrictMembers<A<T>> = bangOptA;
+    const sa5: StrictMembers<A<T>> = strictA;
+    const sa6: StrictMembers<A<T>> = strictOptA;
+
+    const soa1: StrictMembers<OptA<T>> = a;
+    const soa2: StrictMembers<OptA<T>> = optA;
+    const soa3: StrictMembers<OptA<T>> = bangA;
+    const soa4: StrictMembers<OptA<T>> = bangOptA;
+    const soa5: StrictMembers<OptA<T>> = strictA;
+    const soa6: StrictMembers<OptA<T>> = strictOptA;
+}
+
+function extendsTypes<T extends number>(
+    a:          A<T>,
+    optA:       OptA<T>,
+    bangA:      A<T>!,
+    bangOptA:   OptA<T>!,
+    strictA:    StrictMembers<A<T>>,
+    strictOptA: StrictMembers<OptA<T>>
+) {
+    const a1: A<T> = a;
+    const a2: A<T> = optA;
+    const a3: A<T> = bangA;
+    const a4: A<T> = bangOptA;
+    const a5: A<T> = strictA;
+    const a6: A<T> = strictOptA;
+
+    const oa1: OptA<T> = a;
+    const oa2: OptA<T> = optA;
+    const oa3: OptA<T> = bangA;
+    const oa4: OptA<T> = bangOptA;
+    const oa5: OptA<T> = strictA;
+    const oa6: OptA<T> = strictOptA;
+
+    const ba1: A<T>! = a;
+    const ba2: A<T>! = optA;
+    const ba3: A<T>! = bangA;
+    const ba4: A<T>! = bangOptA;
+    const ba5: A<T>! = strictA;
+    const ba6: A<T>! = strictOptA;
+
+    const boa1: OptA<T>! = a;
+    const boa2: OptA<T>! = optA;
+    const boa3: OptA<T>! = bangA;
+    const boa4: OptA<T>! = bangOptA;
+    const boa5: OptA<T>! = strictA;
+    const boa6: OptA<T>! = strictOptA;
+
+    const sa1: StrictMembers<A<T>> = a;
+    const sa2: StrictMembers<A<T>> = optA;
+    const sa3: StrictMembers<A<T>> = bangA;
+    const sa4: StrictMembers<A<T>> = bangOptA;
+    const sa5: StrictMembers<A<T>> = strictA;
+    const sa6: StrictMembers<A<T>> = strictOptA;
+
+    const soa1: StrictMembers<OptA<T>> = a;
+    const soa2: StrictMembers<OptA<T>> = optA;
+    const soa3: StrictMembers<OptA<T>> = bangA;
+    const soa4: StrictMembers<OptA<T>> = bangOptA;
+    const soa5: StrictMembers<OptA<T>> = strictA;
+    const soa6: StrictMembers<OptA<T>> = strictOptA;
+}
+
+function isNonNull<T>(x: T): x is T! {
+    return x !== null;
+}
+
+function test<
+    A extends number | null,
+    B extends { x: string } | null,
+    C extends { x: boolean | null } | null
+>(a: A!, b: B!, c: C, cBang: C!) {
+    const d: number = a;
+    const e: { x: string } = b;
+    const f: { x?: string } = b;
+    const g: { x: number } = b;
+
+    if (isNonNull(c)) {
+        c;
+        c.x;
+        if (isNonNull(c.x)) {
+            c.x;
+        }
+        if (c.x !== null) {
+            c.x;
+        }
+    }
+
+    if (isNonNull(cBang)) {
+        cBang;
+    }
+
+    a = 3;
+}
+
+function f11<T>(x: A<T>!) {
+    x.a;
 }
 
 
@@ -217,6 +372,12 @@ infer5(null);
 infer6({ x: 1, y: 2 });
 infer6({ x: 1, y: 'string' });
 function assignability(x, y) {
+    var a = x;
+    var b = y;
+    var c = x;
+    var d = y;
+}
+function assignability2(x, y) {
     var a = x;
     var b = y;
     var c = x;
@@ -294,6 +455,108 @@ function knownTypes(a, optA, bangA, bangOptA, strictA, strictOptA) {
     var soa5 = strictA;
     var soa6 = strictOptA;
 }
+function unknownTypes(a, optA, bangA, bangOptA, strictA, strictOptA) {
+    var a1 = a;
+    var a2 = optA;
+    var a3 = bangA;
+    var a4 = bangOptA;
+    var a5 = strictA;
+    var a6 = strictOptA;
+    var oa1 = a;
+    var oa2 = optA;
+    var oa3 = bangA;
+    var oa4 = bangOptA;
+    var oa5 = strictA;
+    var oa6 = strictOptA;
+    var ba1 = a;
+    var ba2 = optA;
+    var ba3 = bangA;
+    var ba4 = bangOptA;
+    var ba5 = strictA;
+    var ba6 = strictOptA;
+    var boa1 = a;
+    var boa2 = optA;
+    var boa3 = bangA;
+    var boa4 = bangOptA;
+    var boa5 = strictA;
+    var boa6 = strictOptA;
+    var sa1 = a;
+    var sa2 = optA;
+    var sa3 = bangA;
+    var sa4 = bangOptA;
+    var sa5 = strictA;
+    var sa6 = strictOptA;
+    var soa1 = a;
+    var soa2 = optA;
+    var soa3 = bangA;
+    var soa4 = bangOptA;
+    var soa5 = strictA;
+    var soa6 = strictOptA;
+}
+function extendsTypes(a, optA, bangA, bangOptA, strictA, strictOptA) {
+    var a1 = a;
+    var a2 = optA;
+    var a3 = bangA;
+    var a4 = bangOptA;
+    var a5 = strictA;
+    var a6 = strictOptA;
+    var oa1 = a;
+    var oa2 = optA;
+    var oa3 = bangA;
+    var oa4 = bangOptA;
+    var oa5 = strictA;
+    var oa6 = strictOptA;
+    var ba1 = a;
+    var ba2 = optA;
+    var ba3 = bangA;
+    var ba4 = bangOptA;
+    var ba5 = strictA;
+    var ba6 = strictOptA;
+    var boa1 = a;
+    var boa2 = optA;
+    var boa3 = bangA;
+    var boa4 = bangOptA;
+    var boa5 = strictA;
+    var boa6 = strictOptA;
+    var sa1 = a;
+    var sa2 = optA;
+    var sa3 = bangA;
+    var sa4 = bangOptA;
+    var sa5 = strictA;
+    var sa6 = strictOptA;
+    var soa1 = a;
+    var soa2 = optA;
+    var soa3 = bangA;
+    var soa4 = bangOptA;
+    var soa5 = strictA;
+    var soa6 = strictOptA;
+}
+function isNonNull(x) {
+    return x !== null;
+}
+function test(a, b, c, cBang) {
+    var d = a;
+    var e = b;
+    var f = b;
+    var g = b;
+    if (isNonNull(c)) {
+        c;
+        c.x;
+        if (isNonNull(c.x)) {
+            c.x;
+        }
+        if (c.x !== null) {
+            c.x;
+        }
+    }
+    if (isNonNull(cBang)) {
+        cBang;
+    }
+    a = 3;
+}
+function f11(x) {
+    x.a;
+}
 
 
 //// [nonNull.d.ts]
@@ -319,6 +582,13 @@ declare type Strip1<T> = {
 };
 declare type Strip2<T> = {
     [K in keyof T!]: T![K]!;
+};
+declare type Error1<T, K extends keyof T!> = T[K];
+declare type StupidButOK<T> = {
+    [K in keyof T]: T![K];
+};
+declare type StupidButOK2<T> = {
+    [K in keyof T]: T![K!];
 };
 declare type Obj = {
     x: number | null;
@@ -348,7 +618,6 @@ declare type T16 = MapHasKey<Obj, 'kind'>;
 declare type T17 = Strip1<Obj>;
 declare type T18 = Strip1<Obj | undefined>;
 declare type T19 = Strip2<Obj | undefined>;
-declare type Error1<T, K extends keyof T!> = T[K];
 declare let a: string | undefined | null | never;
 declare let b: typeof a!;
 declare type Assert<T> = T!;
@@ -365,6 +634,7 @@ declare function infer4<T>(x: T, y: T!): void;
 declare function infer5<T>(x: T!): T;
 declare function infer6<T>(x: Record<'x' | 'y', T!>): void;
 declare function assignability<T>(x: T, y: T!): void;
+declare function assignability2<T extends number>(x: T, y: T!): void;
 declare function mappedAssignability<T>(x: T, y: StrictMembers<T>): void;
 declare function comparability<T>(x: T, y: T!): void;
 declare function mappedComparability<T>(x: T, y: StrictMembers<T>): void;
@@ -383,10 +653,19 @@ declare const hg: typeof h;
 declare const wf: typeof w;
 declare const wg: typeof w;
 declare const wh: typeof w;
-declare type A = {
-    a: number;
+declare type A<T> = {
+    a: T;
 };
-declare type OptA = {
-    a: number | null;
+declare type OptA<T> = {
+    a: T | null;
 };
-declare function knownTypes(a: A, optA: OptA, bangA: A!, bangOptA: OptA!, strictA: StrictMembers<A>, strictOptA: StrictMembers<OptA>): void;
+declare function knownTypes(a: A<number>, optA: OptA<number>, bangA: A<number>!, bangOptA: OptA<number>!, strictA: StrictMembers<A<number>>, strictOptA: StrictMembers<OptA<number>>): void;
+declare function unknownTypes<T>(a: A<T>, optA: OptA<T>, bangA: A<T>!, bangOptA: OptA<T>!, strictA: StrictMembers<A<T>>, strictOptA: StrictMembers<OptA<T>>): void;
+declare function extendsTypes<T extends number>(a: A<T>, optA: OptA<T>, bangA: A<T>!, bangOptA: OptA<T>!, strictA: StrictMembers<A<T>>, strictOptA: StrictMembers<OptA<T>>): void;
+declare function isNonNull<T>(x: T): x is T!;
+declare function test<A extends number | null, B extends {
+    x: string;
+} | null, C extends {
+    x: boolean | null;
+} | null>(a: A!, b: B!, c: C, cBang: C!): void;
+declare function f11<T>(x: A<T>!): void;
